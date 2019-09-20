68c3d5f156c7258a3af9165a03a72c92c9a1daa9
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-09-19 21:59:15 -0700

    added space/time complexity

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -13,8 +13,8 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n) where n is the number of elements in the heap
+  # Space Complexity: O(1)
   def add(key, value = key)
     new_node = HeapNode.new(key, value)
     @store << new_node
@@ -25,8 +25,8 @@ class MinHeap
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n) where n is the number of elements in the heap
+  # Space Complexity: O(1)
   def remove()
     swap(0, @store.length - 1)
     root_value = @store.pop.value
@@ -50,8 +50,8 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(n) where n is the number of elements in the array
+  # Space complexity: O(1)
   def empty?
     return @store.length == 0
   end
@@ -61,8 +61,8 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n) where n is the number of elements in the heap
+  # Space complexity: O(1)
   def heap_up(index)
     return if index == 0
     parent_index = (index - 1) / 2
